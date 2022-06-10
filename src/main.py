585067a983f80184fd6f064c50fd0b025cf7d637
Nick Viscomi <nicholasviscomi@gmail.com>
2022-06-10 17:00:14 -0400

    refactored track_green_fiducial to work with the new circle one as well

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import imghdr
 from re import template
 from typing import List, Tuple
 
@@ -38,9 +39,10 @@ upper_square_green = (75,135,130)
 # PATHS FOR SQUAT VIDEOS WITH GREEN CIRCLE FIDUCIAL
 green_circle_video       = 'assets/Green Circle/GreenCircle.mp4'
 circle_template          = 'assets/Green Circle/templates/CircleTemplate.png'
+circle_template2          = 'assets/Green Circle/templates/CircleTemplate2.png'
 isolated_circle_temlpate = 'assets/Green Circle/templates/IsolatedCircle.png'
-lower_circle_green = (60, 110, 140)
-upper_circle_green = (80, 140, 170)
+lower_circle_green = (55, 105, 135)
+upper_circle_green = (85, 145, 175)
 
 
 def analysis(path: str, template_path: str):
@@ -82,7 +84,8 @@ def color_isolated(bgr_img: Mat, lower_color: Tuple, upper_color: Tuple) -> Mat:
 
     return result
 
-def track_green_fiducial(path: str, template_path: str) -> List[List]:
+def track_green_fiducial(path: str, template_path: str, 
+                        lower_color: Tuple[int, int, int], upper_color: Tuple[int, int, int]) -> List[List]:
     # get video strem and template set up
     vid_capture = cv2.VideoCapture(path)
 
@@ -107,7 +110,7 @@ def track_green_fiducial(path: str, template_path: str) -> List[List]:
         ret, frame = vid_capture.read()
         if not ret: break
         original = frame
-        frame = color_isolated(frame, lower_square_green, upper_square_green)
+        frame = color_isolated(frame, lower_color, upper_color)
 
         # match it and draw the rectangle
         match = cv2.matchTemplate(frame, template, method) # for this, cv2.TM_CCORR worked best
@@ -127,89 +130,9 @@ def track_green_fiducial(path: str, template_path: str) -> List[List]:
     vid_capture.release()
     cv2.destroyAllWindows()
     return points
-
-#doesn't work because there are some random pixels that get caught first
-def track_first_green_pixel(path: str) -> List[List]:
-    vid = cv2.VideoCapture(path)
-    if vid.isOpened() == False:
-        print("Error opening video")
-        return
-
-    tracking_points = []
-
-    num_frame = 1
-    while vid.isOpened():
-        ret, frame = vid.read()
-        if ret == False: 
-            print("No more frames to retrieve")
-            break
-        frame = color_isolated(frame, lower_square_green, upper_square_green)
-        # util.display(frame)
-
-        if num_frame % 5 != 0: 
-            # reduce frame rate to lessen number of computations done
-            num_frame += 1
-            continue
-
-        if num_frame == 120:
-            return tracking_points
-
-        # will be set with the previous location of the first green pixel 
-        # so that only the surrounding area is searched and not the whole image
-
-        starting_x, starting_y = 0,0 
-        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        for y in range(starting_y, len(hsv_frame)):
-            for x in range(starting_x, len(hsv_frame[0])):
-                # because of the green isolation, everything else is (0,0,0)
-                # find first pixel with saturation greater than 100
-                if hsv_frame[y][x][1] >= 100: 
-                    print(f"Frame #{num_frame}: non-black pixel @ row {y}, col {x}")
-                    cv2.circle(frame, center=(x, y), radius=5, color=(255,255,255), thickness=-1)
-                    cv2.imshow('Frame', frame)
-                    if cv2.waitKey(1) == ord('q'):
-                        return tracking_points
-
-                    tracking_points.append([y, x])
-
-                    # gives the next iteration an idea of where to start
-                    if y > 200:
-                        starting_y = y - 100
-                    if x > 200:
-                        starting_x = x - 100
-                    break
-            else:
-                # once the inner for loop is exited, this will run
-                # it will go to the line below, and break out of the upper loop
-                continue
-            break
-        
-        num_frame += 1
-
-    return tracking_points
-
-#doesn't work
-def track_circle_fiducial(path: str, template_path: str):
     
-    template = cv2.imread(template_path)
-    # template = color_isolated(template, lower_circle_green, upper_circle_green)
-    template = cv2.cvtColor(template, cv2.COLOR_BGR2HSV)
-
-    vid = cv2.VideoCapture(path)
-    while vid.isOpened():
-        success, frame = vid.read()
-        if not success: break
-
-        frame = color_isolated(frame, lower_circle_green, upper_circle_green)
-
-        # try using template matching on the isolated green circle
-
-        cv2.imshow('Frame', frame)
-        cv2.waitKey(0)
-        break
-            # if cv2.waitKey(1) == ord('q'):
-            #     break
-    cv2.destroyAllWindows()
-
 if __name__ == '__main__':
-    track_green_fiducial(normal_speed_green, bigger_green_fiducial)
+    track_green_fiducial (
+        green_circle_video, isolated_circle_temlpate, 
+        lower_circle_green, upper_circle_green
+    )
